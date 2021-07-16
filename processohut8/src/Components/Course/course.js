c0e7136c34e2ee775072f87f2bddc197ab72c573
Kathe <kathe.isabelle@gmail.com>
2021-07-16 16:13:23 -0300

    atualizações

--- processohut8/src/Components/Course/course.js
+++ processohut8/src/Components/Course/course.js
@@ -4,8 +4,8 @@ import './course.css';
 const Course= () => (
        
     <div className="course">
-        <img src="https://i.imgur.com/aadwyfC.jpg" alt="Imagem Ilustrativa" className="card-image" />
-        <div className="course-underRectangle">
+       
+             <div className="course-underRectangle">
             <p className="course-subject">Cursos Matriculados</p>
             <h1 className="couse-task">Enviar arquivos</h1>
             <p className="course-deadline">05/10/21</p>
